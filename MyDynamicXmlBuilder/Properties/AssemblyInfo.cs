be24cafa3e61a0e6bbc98b9daa0bf2e5598fd2be
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-05 21:43:14 +0200

    update asembly info

--- MyDynamicXmlBuilder/Properties/AssemblyInfo.cs
+++ MyDynamicXmlBuilder/Properties/AssemblyInfo.cs
@@ -2,21 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MyDynamicXmlBuilder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyDynamicXmlBuilder")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Ivan Ivanov 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
--- MyDynamicXmlBuilder/XmlBuilder.cs
+++ MyDynamicXmlBuilder/XmlBuilder.cs
@@ -10,7 +10,7 @@
 
     public class XmlBuilder : DynamicObject
     {
-       
+        
     }
 
 }
