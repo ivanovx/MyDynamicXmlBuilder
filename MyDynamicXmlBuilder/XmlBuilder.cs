eb87c8d8812a3f4174d42acae174efd665df0a51
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-05 21:40:00 +0200

    add depents

--- MyDynamicXmlBuilder/XmlBuilder.cs
+++ MyDynamicXmlBuilder/XmlBuilder.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Xml.Linq;
-using System.Dynamic;
-using System.IO;
-using System.Xml;
-using System.Text;
-
-namespace MyDynamicXmlBuilder
+﻿namespace MyDynamicXmlBuilder
 {
+    using System;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Dynamic;
+    using System.IO;
+    using System.Xml;
+    using System.Text;
 
     public class XmlBuilder : DynamicObject
     {
-        
+       
     }
 
 }
