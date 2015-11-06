14d6c98f69421bbfa3700d71af351412558e1c46
Ivan Ivanov <slayer_word_11@outlook.com>
2015-11-06 11:18:56 +0200

    make project and add example

---
+++ MyDynamicXmlBuilder.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyDynamicXmlBuilder;
+
+namespace MyDynamicXmlBuilder.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic xml = new XmlBuilder();
+
+            xml.hello("word");
+
+            Console.WriteLine(xml.ToString(true));
+        }
+    }
+}
--- MyDynamicXmlBuilder/XmlBuilder.cs
+++ MyDynamicXmlBuilder/XmlBuilder.cs
@@ -10,7 +10,224 @@
 
     public class XmlBuilder : DynamicObject
     {
+        XDocument root = new XDocument();
+        XContainer current;
 
+        public static Action Fragment(Action fragmentBuilder)
+        {
+            if (fragmentBuilder == null)
+            {
+                throw new ArgumentNullException("fragmentBuilder");
+            }
+
+            return fragmentBuilder;
+        }
+
+        public static Action<dynamic> Fragment(Action<dynamic> fragmentBuilder)
+        {
+            if (fragmentBuilder == null)
+            {
+                throw new ArgumentNullException("fragmentBuilder");
+            }
+
+            return fragmentBuilder;
+        }
+
+        public static XmlBuilder Build(Action<dynamic> builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+
+            var xbuilder = new XmlBuilder();
+            builder(xbuilder);
+            return xbuilder;
+        }
+
+        public XmlBuilder()
+        {
+            current = root;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = null;
+            string tagName = binder.Name;
+            Tag(tagName, args);
+            return true;
+        }
+
+        public void Tag(string tagName, params object[] args)
+        {
+            if (String.IsNullOrEmpty(tagName))
+            {
+                throw new ArgumentNullException("tagName");
+            }
+
+            if (tagName.IndexOf('_') == 0)
+                tagName = tagName.Substring(1);
+
+            string content = null;
+            object attributes = null;
+            Action fragment = null;
+
+            args.ToList().ForEach(arg =>
+            {
+                if (arg is Action)
+                    fragment = arg as Action;
+                else if (arg is Action<dynamic>)
+                    fragment = () => (arg as Action<dynamic>)(this);
+
+                else if (arg is string)
+                    content = arg as string;
+
+                else if (arg.GetType().IsValueType)
+                    content = arg.ToString();
+
+                else
+                    attributes = arg;
+            });
+
+            var element = new XElement(tagName);
+            current.Add(element);
+
+            if (fragment != null)
+            {
+                current = element;
+            }
+
+            if (!String.IsNullOrEmpty(content))
+            {
+                element.Add(content);
+            }
+
+            if (attributes != null)
+            {
+                attributes.GetType().GetProperties().ToList().ForEach(prop =>
+                {
+                    if (prop.Name == "xmlns")
+                    {
+                        XNamespace ns = prop.GetValue(attributes, null) as string;
+                        element.Name = ns + element.Name.ToString();
+                    }
+                    else
+                    {
+                        element.Add(new XAttribute(prop.Name, prop.GetValue(attributes, null)));
+                    }
+                });
+            }
+
+            if (fragment != null)
+            {
+                fragment();
+                current = element.Parent;
+            }
+        }
+
+        public void Comment(string comment)
+        {
+            if (String.IsNullOrEmpty(comment)) { throw new ArgumentNullException("comment"); }
+
+            current.Add(new XComment(comment));
+        }
+
+        public void CData(string data)
+        {
+            if (String.IsNullOrEmpty(data)) { throw new ArgumentNullException("data"); }
+
+            current.Add(new XCData(data));
+        }
+
+        public void Text(string text)
+        {
+            if (String.IsNullOrEmpty(text)) { throw new ArgumentNullException("text"); }
+
+            current.Add(new XText(text));
+        }
+
+        public void Declaration(string version = null, string encoding = null, string standalone = null)
+        {
+            root.Declaration = new XDeclaration(version, encoding, standalone);
+        }
+
+        public void DocumentType(string name, string publicId = null, string systemId = null, string internalSubset = null)
+        {
+            if (String.IsNullOrEmpty(name)) { throw new ArgumentNullException("name"); }
+
+            root.Add(new XDocumentType(name, publicId, systemId, internalSubset));
+        }
+
+        #region converters
+
+        public static implicit operator string (XmlBuilder xml)
+        {
+            return xml.ToString(false);
+        }
+
+        public string ToString(bool indent)
+        {
+            Encoding encoding = new UTF8Encoding(false);
+
+            if (root.Declaration != null &&
+                !String.IsNullOrEmpty(root.Declaration.Encoding) &&
+                root.Declaration.Encoding.ToLowerInvariant() == "utf-16")
+                encoding = new UnicodeEncoding(false, false);
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            XmlWriter xmlWriter = XmlWriter.Create(memoryStream, new XmlWriterSettings
+            {
+                Encoding = encoding,
+                Indent = indent,
+                CloseOutput = true,
+
+                OmitXmlDeclaration = root.Declaration == null
+            });
+            root.Save(xmlWriter);
+            xmlWriter.Flush();
+            xmlWriter.Close();
+
+            if (encoding is UnicodeEncoding)
+                return Encoding.Unicode.GetString(memoryStream.ToArray());
+            else
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+        }
+
+
+        public XDocument ToXDocument()
+        {
+            return root;
+        }
+
+        public XElement ToXElement()
+        {
+            return root.Elements().FirstOrDefault();
+        }
+
+        public XmlDocument ToXmlDocument()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(root.CreateReader());
+            return xmlDoc;
+        }
+
+        public XmlNode ToXmlNode()
+        {
+            if (root.DocumentType != null && root.Nodes().Count() > 1)
+                return ToXmlDocument().ChildNodes[1] as XmlNode;
+            else if (root.DocumentType == null && root.Nodes().Count() >= 1)
+                return ToXmlDocument().FirstChild as XmlNode;
+            else
+                return null as XmlNode;
+        }
+
+        public XmlElement ToXmlElement()
+        {
+            return ToXmlNode() as XmlElement;
+        }
+
+        #endregion
     }
 
 }
