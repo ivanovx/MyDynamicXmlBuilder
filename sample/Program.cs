c8eb499a701a057236533701d231b49cc0f5d585
csyntax <csyntax@outlook.com>
2015-12-23 18:26:39 +0200

    update demo

--- sample/Program.cs
+++ sample/Program.cs
@@ -12,12 +12,22 @@ namespace MyDynamicXmlBuilder.Example
 
             xml.Declaration();
 
-            xml.user("Kiro Zlatniq", new
-            {
-                id = 1,
-                username = "kiro",
-                age = 50,
-            });
+            /* xml.user("Kiro Zlatniq", new
+             {
+                 id = 1,
+                 username = "kiro",
+                 age = 50,
+             });*/
+
+
+            xml.user(XmlBuilder.Fragment(u => {
+                u.firstname("Kiro");
+                u.lastname("Zlatnia");
+                u.email("kiro@zlatnia.bg");
+                u.phone(new { type = "cell" }, "(985) 555-1234");
+            }));
+
+
 
             Console.WriteLine(xml.ToString(true));
         }
