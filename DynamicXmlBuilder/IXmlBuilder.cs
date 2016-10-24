80307bb3b17f49bfe4e036819d8b51f9bc2ef317
csyntax <csyntax@outlook.com>
2016-10-24 23:06:14 +0300

    shared code

---
+++ DynamicXmlBuilder/IXmlBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Dynamic;
+using System.Xml.Serialization;
+
+namespace DynamicXmlBuilder
+{
+    internal interface IXmlBuilder : IDisposable, IDynamicMetaObjectProvider
+    {
+
+    }
+}
---
+++ DynamicXmlBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DynamicXmlBuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamicXmlBuilder")]
+[assembly: AssemblyCopyright("Copyright ©  2015 - 2017 Ivan Ivanov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
