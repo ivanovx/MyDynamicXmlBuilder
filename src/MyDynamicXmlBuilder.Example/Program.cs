6c3b3eb37e009ef613853cbcd24eea82ca17c6f6
Ivan Ivanov <csyntax@outlook.com>
2017-03-13 16:59:02 +0200

    clear and update demo

--- src/MyDynamicXmlBuilder.Example/Program.cs
+++ src/MyDynamicXmlBuilder.Example/Program.cs
@@ -28,7 +28,7 @@ namespace MyDynamicXmlBuilder.Example
                     }));
                 }));
 				
-				Console.WriteLine(xml);
+				Console.WriteLine(xml.Build());
             }           
         }
 	}
